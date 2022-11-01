8984fb21ca60f9c7b3fcb57f406d9553e8f87274
clandinq <cesar.landin@itam.mx>
2022-11-01 12:03:57 -0500

    Add pictures

--- scripts/prepare_timesheet.R
+++ scripts/prepare_timesheet.R
@@ -2,39 +2,43 @@
 # File name: 		      prepare_timesheet.R
 # Creation date:      2022-04-19
 # Author:          		César Landín
-# Files used:
-#   - CSV detailed report from Clockify in Downloads folder.
-# Files created:
-#   - current_timesheet.csv in Downloads folder.
 # Purpose:
 # 	- Prepare timesheet.
 #********************************************************************************************
 
 #***************** Import packages *****************#
-if (!require(pacman)) {install.packages("pacman")}
+suppressPackageStartupMessages(
+  if (!require(pacman)) {install.packages("pacman")}
+)
 pacman::p_load(here, tidyverse, magrittr, lubridate)
 #*************************************************** #
 
-######################################################
-##    (1): Process most recent Clockify timesheet.  ##
-######################################################
-# (1.1): Define where the downloads folder is located. #
-downloads_folder <- "/Users/myusername/Downloads/"
-  
-# (1.2): Read Clockify report download. #
+################################
+##    (1): Prepare timesheet.  #
+################################
+# (1.1): Identify Clockify download. #
+downloads_folder <- here("/Users/cesarlandin/Downloads/")
 current_file <- list.files(downloads_folder, pattern = "*.csv")
 current_file <- current_file[str_detect(current_file, "Clockify")]
-raw <- read_csv(paste0(downloads_folder, current_file))
-colnames(raw) <- colnames(raw) %>% str_to_lower() %>% str_replace(" ", "_")
 
-# (1.3): Process variables and sort data by date. #
+# (1.2): Read file. #
+raw <- read_csv(str_c(downloads_folder, current_file),
+                show_col_types = FALSE) %>% 
+  rename_all(~str_to_lower(.) %>% str_replace(" ", "_"))
+
+# (1.3): Process timesheet. #
 proc <- raw %>% 
+  # Keep reduced set of variables
   select(start_date, start_time, end_time, task, description) %>% 
+  # Change parsing to ensure compatibility with Excel
   mutate_at(vars(contains("time")), ~format(parse_date_time(., c("HMS", "HM")), "%H:%M")) %>% 
+  # Calculate duraction (best practice is to extend formula in Excel either way)
   mutate(duration = difftime(strptime(end_time, format = "%H:%M"), strptime(start_time, format = "%H:%M"), units = "hours"),
          duration = round(duration, 1)) %>% 
+  # Arrange and relocate variables (match timesheet formatting)
   arrange(start_date, start_time) %>% 
   relocate(duration, .after = end_time)
 
-# (1.4): Save processed report to copy to timesheet. #
-write_csv(proc, paste0(downloads_folder, "current_timesheet.csv"))
+# (1.4): Save processed timesheet. #
+write_csv(proc, str_c(downloads_folder, "current_timesheet.csv"))
+# Final step: copy and paste this into timesheet Excel.
